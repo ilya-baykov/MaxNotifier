dc1319c07567f1d50eb832625ad82ebda8deb8c6
ilyab <i-baykov@internet.ru>
2025-09-07 21:16:01 +0200

    Синхронный и асинхронный Producer

--- producer.py
+++ producer.py
@@ -1,32 +1,59 @@
 import json
+import asyncio
 
+# --- Синхронный Producer ---
 from pika import ConnectionParameters, BlockingConnection
 
-connection_params = ConnectionParameters(
-    host='localhost',
-    port=5672
-)
-message = {
-    "chat_id": 793353522,  # твой chat_id
-    "message_text": "Привет, это тестовое сообщение!"
-}
+class Producer:
+    def __init__(self, host='localhost', port=5672, queue_name='max_notifications'):
+        self.connection_params = ConnectionParameters(host=host, port=port)
+        self.queue_name = queue_name
+
+    def send(self, message: dict):
+        """Отправка сообщения в очередь (синхронно)"""
+        with BlockingConnection(self.connection_params) as connection:
+            with connection.channel() as channel:
+                channel.queue_declare(queue=self.queue_name, durable=True)
+                channel.basic_publish(
+                    exchange='',
+                    routing_key=self.queue_name,
+                    body=json.dumps(message)
+                )
+                print(f'[SYNC] Сообщение отправлено в очередь {self.queue_name}')
 
 
-def main():
-    with BlockingConnection(connection_params) as connection:
-        with connection.channel() as channel:
-            queue_name = 'max_notifications'
+# --- Асинхронный AsyncProducer ---
+import aio_pika
 
-            channel.queue_declare(queue=queue_name, durable=True)  # Опционально: убедимся, что очередь существует
+class AsyncProducer:
+    def __init__(self, url='amqp://guest:guest@localhost/', queue_name='max_notifications'):
+        self.url = url
+        self.queue_name = queue_name
 
-            # Отправка сообщения в очередь
-            channel.basic_publish(
-                exchange='',  # пустой exchange → default
-                routing_key=queue_name,  # имя очереди
-                body=json.dumps(message)
+    async def send(self, message: dict):
+        """Отправка сообщения в очередь (асинхронно)"""
+        connection = await aio_pika.connect_robust(self.url)
+        async with connection:
+            channel = await connection.channel()
+            queue = await channel.declare_queue(self.queue_name, durable=True)
+            await channel.default_exchange.publish(
+                aio_pika.Message(body=json.dumps(message).encode()),
+                routing_key=self.queue_name
             )
-            print(f'Сообщение отправлено в очередь {queue_name}')
+            print(f'[ASYNC] Сообщение отправлено в очередь {self.queue_name}')
 
 
+# --- Пример использования ---
 if __name__ == '__main__':
-    main()
+    message = {"chat_id": 793353522, "message_text": "Привет, это тестовое сообщение!"}
+
+    # # Синхронный Producer
+    # producer = Producer()
+    # producer.send(message)
+
+    # Асинхронный Producer
+    async def async_main():
+        async_producer = AsyncProducer()
+        await async_producer.send(message)
+
+    asyncio.run(async_main())
